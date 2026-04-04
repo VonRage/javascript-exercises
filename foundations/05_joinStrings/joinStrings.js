1052316c38d9f72127286eacdf2934ee2d0df4b6
Tyler Roth <134968282+VonRage@users.noreply.github.com>
2026-04-03 22:13:45 -0400

    Ignore commit, need error in code

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -13,7 +13,7 @@ let firstName = "Carlos";
 let lastName = "Stevenson";
 let birthYear = 1947;
 let thisYear = 1965;
-let fullName = firstName + ' ' + lastName;
+let fullName = firstName + lastName;
 let age = thisYear - birthYear
 let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
 
